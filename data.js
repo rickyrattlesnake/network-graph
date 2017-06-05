b440aae0d13978f94abd7cb46ea51c1214d8c300
rattlesnake <ricky.ratnayake@gmail.com>
2017-06-06 04:29:42 +1000

    variable dimensions and x axis distribution

---
+++ data.js
@@ -0,0 +1,54 @@
+let data = {
+    nodes: [
+        {
+            id: 'n1',
+            payload: {
+                type: 'router',
+            },
+        },
+        {
+            id: 'n2',
+            payload: {
+                type: 'router',
+            }
+        },
+        {
+            id: 'n3',
+            payload: {
+                type: 'router',
+            },
+        },
+        {
+            id: 'n4',
+            payload: {
+                type: 'router',
+            }
+        },
+    ],
+    edges: [
+        {
+            id: 'e1',
+            source: 'n1',
+            dest: 'n2',
+            payload: {
+                latency: '1000',
+            }
+        },
+        {
+            id: 'e1',
+            source: 'n2',
+            dest: 'n3',
+            payload: {
+                latency: '1000',
+            }
+        },
+        {
+            id: 'e1',
+            source: 'n3',
+            dest: 'n4',
+            payload: {
+                latency: '1000',
+            }
+        },
+    ]
+};
--- network-graph.js
+++ network-graph.js
@@ -1,31 +1,51 @@
 'use strict';
 
 let CONTAINER_ID = 'container';
-// let CONTAINER_WIDTH = 300;
-// let CONTAINER_HEIGHT = 200;
+let VIEWBOX_WIDTH = 600;
+let VIEWBOX_HEIGHT = 300;
+let NODE_HEIGHT = 25;
+let NODE_WIDTH = 25;
 
-let nodeData = [
-    {
-        label: 'A',
-        type: 'router',
-        x: 200,
-        y: 150,
-    },
-    {
-        label: 'B',
-        type: 'router',
-        x: 300,
-        y: 150,
-    },
-];
-let edgeData = [
-    {
-        sourceNode: 'A',
-        destNode: 'B',
-        latency: '1000',
-    },
-];
 
+let nodeData = data.nodes;
+let edgeData = data.edges;
+
+
+// Vertically centre the nodes
+let nodeYPos = 1/2 * (VIEWBOX_HEIGHT - NODE_HEIGHT);
+// Horizontally distribute nodes with equal spacing
+let numNodes = nodeData.length;
+let spacing = (VIEWBOX_WIDTH - (numNodes * NODE_WIDTH)) / (numNodes + 1);
+let nodeXPos = [];
+for (let i = 0; i < nodeData.length; i++) {
+    if (i === 0) {
+        nodeXPos[i] = spacing;
+    } else {
+        nodeXPos[i] = spacing + i * (spacing + NODE_WIDTH);
+    }
+
+    //Integrate positions with data
+    nodeData[i].x = nodeXPos[i];
+    nodeData[i].y = nodeYPos;
+}
+
+//
+for (let i = 0; i < edgeData.length; i++){
+    let edge = edgeData[i];
+    let source = nodeData.filter((node) => {
+        return node.id === edge.source;
+    })[0];
+    let dest = nodeData.filter((node) => {
+        return node.id === edge.dest;
+    })[0];
+
+    // Position it halfwaydown on right edge
+    edge.x1 = source.x + NODE_WIDTH;
+    edge.y1 = source.y + NODE_HEIGHT / 2;
+    // Halfway down left edge
+    edge.x2 = dest.x;
+    edge.y2 = dest.y + NODE_HEIGHT / 2;
+}
 
 
 let c10 = (i) => { return d3.schemeCategory10[i % 10]; };
@@ -33,30 +53,17 @@ let svg = d3.select('body')
     .append('svg')
     .attr('viewBox', '0 0 600 300')
     .attr('preserveAspectRatio', 'xMidYMid meet');
-    // .attr('width', CONTAINER_WIDTH)
-    // .attr('height', CONTAINER_HEIGHT);
 
 let links = svg.selectAll('link')
     .data(edgeData)
     .enter()
     .append('line')
     .attr('class', 'link')
-    .attr('x1', function(edge){
-        let sourceNode = nodeData.filter((node) => {
-            return node.label === edge.sourceNode;
-        })[0];
+    .attr('x1', function(edge) { return edge.x1; })
+    .attr('y1', function(edge) { return edge.y1; })
+    .attr('x2', function(edge) { return edge.x2; })
+    .attr('y2', function(edge) { return edge.y2; });
 
-        d3.select(this).attr('y1', sourceNode.y);
-        return sourceNode.x;
-    })
-    .attr('x2', function(edge){
-        let destNode = nodeData.filter((node) => {
-            return node.label === edge.destNode;
-        })[0];
-
-        d3.select(this).attr('y2', destNode.y);
-        return destNode.x - 30;
-    });
 
 let nodes = svg.selectAll('node')
     .data(nodeData)
@@ -64,10 +71,10 @@ let nodes = svg.selectAll('node')
     .append("use")
     .attr("xlink:href", function() { return "#router"; } )
     .attr('class', 'node')
-    .attr('x', function (node){ return node.x - 25; })
-    .attr('y', function (node){ return node.y - 10; })
-    .attr('height', 20)
-    .attr('width', 20)
+    .attr('x', function (node){ return node.x; })
+    .attr('y', function (node){ return node.y; })
+    .attr('height', NODE_HEIGHT)
+    .attr('width', NODE_WIDTH)
     .attr('fill', function (d, i){ return c10(i); });
 
 var edgelabels = svg.selectAll("text")
